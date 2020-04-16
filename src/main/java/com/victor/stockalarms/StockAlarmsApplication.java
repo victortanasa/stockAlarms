f5e06c95ce927027a3f1fbd46624566366756e44
Victor Tanasa <victor.tanasa@muso.com>
2020-04-16 15:22:48 +0300

    initial commit

---
+++ src/main/java/com/victor/stockalarms/StockAlarmsApplication.java
@@ -0,0 +1,13 @@
+package com.victor.stockalarms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAlarmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAlarmsApplication.class, args);
+	}
+
+}
