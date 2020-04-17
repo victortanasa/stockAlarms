f0a90a47ea1e8e12033dff64434fa502c74bce03
Victor Tanasa <victor.tanasa@muso.com>
2020-04-17 19:38:26 +0300

    refactor exception handler

--- src/main/java/com/victor/stockalarms/controller/ControllerExceptionHandler.java
+++ src/main/java/com/victor/stockalarms/controller/ControllerExceptionHandler.java
@@ -5,23 +5,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.persistence.EntityNotFoundException;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(DataAccessException.class)
     public ResponseEntity<String> handleDataAccessException(final DataAccessException e) {
-        return ResponseEntity.badRequest().body(e.getMostSpecificCause().getMessage());
+        return new ResponseEntity<>(e.getMostSpecificCause().getMessage(), HttpStatus.BAD_REQUEST);
     }
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<String> handleEntityNotFoundException(final EntityNotFoundException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
 
 }
